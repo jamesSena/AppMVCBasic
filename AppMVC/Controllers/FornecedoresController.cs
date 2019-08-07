eb9ab8b964a25c4edbde34b3b65f030520351117
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-08-07 14:24:49 -0300

    Gerando a tela de Fornecedores via "MVC controller with views, using Entity Framework"

---
+++ AppMVC/Controllers/FornecedoresController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AppMVC.Data;
+using AppMVC.Models;
+
+namespace AppMVC.Controllers
+{
+    public class FornecedoresController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FornecedoresController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Fornecedors
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Fornecedores.ToListAsync());
+        }
+
+        // GET: Fornecedors/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fornecedor = await _context.Fornecedores.FirstOrDefaultAsync(m => m.ID == id);
+            if (fornecedor == null)
+            {
+                return NotFound();
+            }
+
+            return View(fornecedor);
+        }
+
+        // GET: Fornecedors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Fornecedors/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Fornecedor fornecedor)
+        {
+            if (ModelState.IsValid)
+            {
+                fornecedor.ID = Guid.NewGuid();
+                _context.Add(fornecedor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(fornecedor);
+        }
+
+        // GET: Fornecedors/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fornecedor = await _context.Fornecedores.FindAsync(id);
+            if (fornecedor == null)
+            {
+                return NotFound();
+            }
+            return View(fornecedor);
+        }
+
+        // POST: Fornecedors/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, Fornecedor fornecedor)
+        {
+            if (id != fornecedor.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(fornecedor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FornecedorExists(fornecedor.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(fornecedor);
+        }
+
+        // GET: Fornecedors/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fornecedor = await _context.Fornecedores
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (fornecedor == null)
+            {
+                return NotFound();
+            }
+
+            return View(fornecedor);
+        }
+
+        // POST: Fornecedors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var fornecedor = await _context.Fornecedores.FindAsync(id);
+            _context.Fornecedores.Remove(fornecedor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FornecedorExists(Guid id)
+        {
+            return _context.Fornecedores.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ AppMVC/Migrations/20190807170425_TabelasSistema2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AppMVC.Migrations
+{
+    public partial class TabelasSistema2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
