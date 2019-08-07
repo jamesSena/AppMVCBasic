9ba2c114df1981aa3231aa6d76fc0412058c02e1
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-08-07 13:32:37 -0300

    Validações no Model. Comando "Add-Migration" Executado com sucesso.

--- AppMVC/Models/Endereco.cs
+++ AppMVC/Models/Endereco.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,8 @@ namespace AppMVC.Models
     {
         public Fornecedor Fornecedor { get; set; }
         public Guid FornecedorId { get; set; }
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 3)]
         public string Logradouro { get; set; }
         public string Numero { get; set; }
         public string Complemento { get; set; }
